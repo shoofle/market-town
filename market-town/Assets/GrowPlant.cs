ad19efa40033b7f1fb0c3761b92105b895c77529
Shoofle Munroe <shoofle@thelevelup.com>
2015-05-31 15:16:52 -0400

    Changed everything like a punk òuó

--- market-town/Assets/GrowPlant.cs
+++ market-town/Assets/GrowPlant.cs
@@ -2,29 +2,40 @@
 using System;
 
 public class GrowPlant : MonoBehaviour {
+	// How much time between the plant growing a stage.
+	public float secondsBetweenGrowth = 60f;
 
-	// The state of the plant; 1 is seed
-	private int state;
+	// The state of the plant; 0 is seed
+	public int state = 0;
+
+	// The sprites to use to render things
+	public Sprite[] sprites;
+
 	private TimeSpan timeAtStateChange;
 	private string plantname;
-	public GameObject plantObject;
+	private SpriteRenderer spriteRenderer;
 	
 	// Use this for initialization
 	private void Start () {
-		this.state = 1;
 		timeAtStateChange = DateTime.Now.TimeOfDay;
 		plantname = "Blue_Pluto";
+
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		spriteRenderer.sprite = sprites [0];
 	}
 	
 	// Update is called once per frame
 	private void Update () {
 		TimeSpan diff = DateTime.Now.TimeOfDay - timeAtStateChange;
-		if (diff.Minutes >= 1) {
-			if(state != 4)
+		if (diff.Seconds > secondsBetweenGrowth) {
+			if(state < sprites.Length - 1)
 			{
 				// change state set texture
 				state++;
 
+				timeAtStateChange = DateTime.Now.TimeOfDay;
+
+				spriteRenderer.sprite = sprites[state];
 			}
 
 		}
