27eec13fcd5ffcc0a6385c320bd99014911e2772
Shoofle Munroe <shoofle@thelevelup.com>
2015-05-31 10:53:08 -0400

    AND THE STUFF

---
+++ market-town/Assets/PlantHolder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantHolder : MonoBehaviour {
+	public GameObject whatsPlantedInMe;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ market-town/Assets/PlantLife.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantLife : MonoBehaviour {
+	int growthState = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (Random () > 0.5) {
+//			growthState++;
+//		}
+	}
+}
