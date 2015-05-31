aa3f45b8dd6d2ae04d4ff4d90bfb03d917fa74e1
Toss <cactoss@users.noreply.github.com>
2015-05-31 13:35:28 -0400

    Plant stuff

---
+++ market-town/Assets/GrowPlant.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+
+public class GrowPlant : MonoBehaviour {
+
+	// The state of the plant; 1 is seed
+	private int state;
+	private TimeSpan timeAtStateChange;
+	private string plantname;
+	public GameObject plantObject;
+	
+	// Use this for initialization
+	private void Start () {
+		this.state = 1;
+		timeAtStateChange = DateTime.Now.TimeOfDay;
+		plantname = "Blue_Pluto";
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		TimeSpan diff = DateTime.Now.TimeOfDay - timeAtStateChange;
+		if (diff.Minutes >= 1) {
+			if(state != 4)
+			{
+				// change state set texture
+				state++;
+
+			}
+
+		}
+	}
+}
