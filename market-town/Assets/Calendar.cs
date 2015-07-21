0abd6cd21dc6281f050b5f66fdae09a5721e944f
Toss <cactoss@users.noreply.github.com>
2015-07-21 17:30:35 -0400

    committing so i can PULL

---
+++ market-town/Assets/Calendar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Calendar : MonoBehaviour {
+
+	#region public
+	// Hour of the day for Plants to access
+	public int DayOfSeason;
+	
+	// Minute of the Hour for Clock to access
+	public string Season;
+	#endregion
+
+	#region private
+	private GameObject go; 
+
+	private DirectDay light;
+	#endregion
+
+
+	// Use this for initialization
+	void Start () {
+		go = GameObject.Find("DirectionalLight");
+		light = go.GetComponent<DirectDay>();
+		DayOfSeason = 1;
+		Season = "Spring";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
