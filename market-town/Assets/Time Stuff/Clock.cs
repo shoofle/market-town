a0c78dbaa8bf6f604803c6fedf004e4d4515b495
Toss <cactoss@users.noreply.github.com>
2015-07-24 18:41:19 -0400

    map shit yesahgf

---
+++ market-town/Assets/Time Stuff/Clock.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Clock : MonoBehaviour
+{
+
+	// ??
+	void OnGUI ()
+	{
+		GameObject go = GameObject.Find ("DirectionalLight");
+		DirectDay light = go.GetComponent<DirectDay> ();
+		Calendar calendar = go.GetComponent<Calendar> ();
+
+		float t = light.currentTime;
+		string hour = LeadingZero ((int)Math.Floor (t));
+		string minute = LeadingZero ((int)((t - Math.Floor (t)) * 60));
+
+		GUILayout.Label (hour + ":" + minute 
+			+ " " + calendar.DayOfSeason 
+			+ " " + calendar.Season
+			+ " " + calendar.CurrentTemperature + "°F"
+			+ " " + calendar.CurrentPrecipitation);
+	}
+
+	// Adds Zeros to number if needed
+	private string LeadingZero (int n)
+	{
+		return n.ToString ().PadLeft (2, '0');
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
