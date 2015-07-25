c1d0467ff1dc65c5a42947c8c15c00c1c8cf3147
Toss <cactoss@users.noreply.github.com>
2015-07-25 08:45:45 -0400

    gui

--- market-town/Assets/Time Stuff/Clock.cs
+++ market-town/Assets/Time Stuff/Clock.cs
@@ -4,23 +4,30 @@ using System;
 
 public class Clock : MonoBehaviour
 {
+	private GameObject directGo;
+	private GameObject guiTime;
+	private GameObject guiTemp;
+
+	private DirectDay light;
+	private Calendar calendar;
 
 	// ??
 	void OnGUI ()
 	{
-		GameObject go = GameObject.Find ("DirectionalLight");
-		DirectDay light = go.GetComponent<DirectDay> ();
-		Calendar calendar = go.GetComponent<Calendar> ();
-
 		float t = light.currentTime;
 		string hour = LeadingZero ((int)Math.Floor (t));
 		string minute = LeadingZero ((int)((t - Math.Floor (t)) * 60));
 
-		GUILayout.Label (hour + ":" + minute 
-			+ " " + calendar.DayOfSeason 
-			+ " " + calendar.Season
-			+ " " + calendar.CurrentTemperature + "°F"
-			+ " " + calendar.CurrentPrecipitation);
+		// Debug.Log("in on gui");
+		// GUIText text = guiTime.GetComponent<GUIText> ();
+		// text.text = hour + ":" + minute 
+		// 	+ " " + calendar.Season 
+		// 	+ " " + calendar.DayOfSeason;
+
+		// text =  guiTemp.GetComponent<GUIText> ();
+
+		// text.text = calendar.CurrentTemperature + "°F"
+		// 	+ " " + calendar.CurrentPrecipitation;
 	}
 
 	// Adds Zeros to number if needed
@@ -32,7 +39,16 @@ public class Clock : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-	
+		Debug.Log("in start gui");
+
+		directGo = GameObject.Find ("DirectionalLight");
+		light = directGo.GetComponent<DirectDay> ();
+		calendar = directGo.GetComponent<Calendar> ();
+
+		guiTime = GameObject.Find ("TimeLabel");
+		guiTemp = GameObject.Find ("TemperatureLabel");
+
+		Debug.Log (guiTemp.GetType ());
 	}
 	
 	// Update is called once per frame
